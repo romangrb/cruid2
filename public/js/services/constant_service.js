a2042922fc73db0ba41422f5c2d343003dd25186
hadouke <d_com@ukr.net>
2016-04-02 14:26:26 +0000

    provided img name filter

--- public/js/services/constant_service.js
+++ public/js/services/constant_service.js
@@ -5,7 +5,12 @@
   angular.module('galleryApp')
     .constant('constant', {
       
-      UPLOAD_URL : 'https://cruid2-romangrb.c9users.io/upload'
+      UPLOAD_URL : 'https://cruid2-romangrb.c9users.io/upload',
+      
+      MAX_IMG_NAME_LN : 30,
+      DFLT_IMG_NAME_START_LN : 17,
+      DFLT_IMG_NAME_DEV :  '...',
+      DFLT_IMG_STR_TYPE_ERR_MSG : 'the name is not a string type',
       
     });
     
