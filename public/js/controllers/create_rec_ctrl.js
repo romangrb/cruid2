265e727743342ba4b09444482bd8e00500887177
romangrb <grabarroman@gmail.com>
2016-04-18 18:48:02 +0000

    add css dropbox style

--- public/js/controllers/create_rec_ctrl.js
+++ public/js/controllers/create_rec_ctrl.js
@@ -139,6 +139,27 @@
         // for directive eather drag is supported or not
         $scope.isDroppable = true;
         
+        
+        
+        $('.modal-trigger').leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+            ready: function() { alert('Ready'); }, // Callback for Modal open
+            complete: function() { alert('Closed'); } // Callback for Modal close
+          }
+        );
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }]);
 
 })(angular);
