cc0b5184729c64dc4e7e041d66761bc1db712b18
romangrb <grabarroman@gmail.com>
2016-04-18 19:38:30 +0000

    provide module listener

--- public/js/controllers/create_rec_ctrl.js
+++ public/js/controllers/create_rec_ctrl.js
@@ -1,4 +1,4 @@
-(function(angular){
+(function(angular, $){
   
   "use strict";
   
@@ -141,16 +141,51 @@
         
         
         
-        $('.modal-trigger').leanModal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            in_duration: 300, // Transition in duration
-            out_duration: 200, // Transition out duration
-            ready: function() { alert('Ready'); }, // Callback for Modal open
-            complete: function() { alert('Closed'); } // Callback for Modal close
-          }
-        );
+        $scope.trumbImg = '';
+        $scope.trumbCroppedImg = '';
         
+        
+        $scope.closeModule = function(){
+          
+          console.log($scope.trumbCroppedImg);
+          $('#modal1').closeModal();
+          
+        };
+      
+        $scope.openModule = function(data){
+          
+          $('#modal1').openModal();
+       
+          var getThumbnaiView = function(fdata){
+          
+          var file=fdata,
+           reader = new FileReader();
+          
+          reader.onload = function (evt) {
+            $scope.$apply(function($scope){
+              $scope.trumbImg=evt.target.result;
+            });
+          };
+          reader.readAsDataURL(file);
+        };
+        
+          if (data && Object.keys(data).length!=0) getThumbnaiView(data);
+           
+          angular.element(document.querySelector('#fileInput')).on('change',getThumbnaiView);
+          
+          $scope.loadBegin = function(){
+            console.info('Crop begin');   
+          };
+          
+          $scope.loadDone = function(){
+            console.info('Crop done');   
+          };
+          
+          $scope.loadError = function(){
+            console.warn('Crop Errror');   
+          };
+        
+        };
         
         
         
@@ -162,4 +197,4 @@
         
     }]);
 
-})(angular);
+})(angular, jQuery);
--- public/js/controllers/crop_ctrl.js
+++ public/js/controllers/crop_ctrl.js
@@ -1,4 +1,4 @@
-(function(angular){
+(function(angular, $){
   
   "use strict";
   
@@ -54,7 +54,25 @@
         $scope.loadError = function(){
           console.warn('Crop Errror');   
         };
+        
+        
+        
+        $('.modal-trigger').leanModal({
+          dismissible: true, // Modal can be dismissed by clicking outside of the modal
+          opacity: .5, // Opacity of modal background
+          in_duration: 300, // Transition in duration
+          out_duration: 200, // Transition out duration
+          ready: function() { alert('Ready'); }, // Callback for Modal open
+          complete: function() { alert('Closed'); } // Callback for Modal close
+        }
+        );
+        
+        
+        
+        
+        
+        
      
     }]);
 
-})(angular);
+})(angular, jQuery);
